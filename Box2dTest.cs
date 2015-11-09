f1355b69a750a43e202f4e9c2584ec90045b97fa
jian.z <36326068@qq.com>
2015-11-09 21:54:33 +0800

    up

--- Box2dTest.cs
+++ Box2dTest.cs
@@ -23,27 +23,47 @@ public class Box2dTest : MonoBehaviour {
 	// d. create fixture  
 	groundBody->CreateFixture(&groundBox, 0.0f);    // 0.0f is density, for static body, mass is zero, so density is not used.  
 	 */
-	List<Body> bodyList = new List<Body>();
+    class Box{
+        public GameObject go;
+        public Body body;
+        public float width, height;
+        public Box(Body body, float hwidth,float hheight) {
+            this.body = body;
+            this.width = hwidth*2;
+            this.height = hheight*2;
+            go = BoxPool.instance.Get();
+            go.transform.localScale = new Vector3(width, height, 1);
+        }
+        public void Update() {
+            go.transform.position = body.GetPosition();
+            go.transform.eulerAngles = new Vector3(0,0, body.GetAngle());
+        }
+    }
+
+    List<Box> boxList = new List<Box>();
 	World world;
+    Body testBody;
+
 	// Use this for initialization
-	void Start () {
-		
-		AABB aabb = new AABB();
-		aabb.UpperBound = new UnityEngine.Vector2(1000,1000);
-		aabb.LowerBound = new UnityEngine.Vector2(-1000,-1000);
-		world = new World(aabb, new Vector2(0, -10), false);
+    void Start()
+    {
 
-		CreateBox (0, 0, 10, 10,true);
-		CreateBox (0, 5, 1, 1,false);
+        AABB aabb = new AABB();
+        aabb.UpperBound = new UnityEngine.Vector2(1000, 1000);
+        aabb.LowerBound = new UnityEngine.Vector2(-1000, -1000);
+        world = new World(aabb, new Vector2(0, -10), false);
 
-	}
+       testBody= CreateBox(0, 0, 10, 1, false);
+         CreateBox(0, 5, 1, 1, false);
 
-	void CreateBox(float x,float y,float hwidth,float hheight,bool isStatic){
+    }
+
+	Body CreateBox(float x,float y,float hwidth,float hheight,bool isStatic){
 		BodyDef groundBodyDef = new BodyDef ();
 		groundBodyDef.Position.Set (x, y);
-		
+        groundBodyDef.FixedRotation = true;
+        
 		Body body = world.CreateBody (groundBodyDef);
-		bodyList.Add (body);
 		PolygonDef groundBox = new PolygonDef ();
 		groundBox.SetAsBox (hwidth, hheight);
 		Fixture fixture = body.CreateFixture (groundBox);
@@ -53,36 +73,55 @@ public class Box2dTest : MonoBehaviour {
 			fixture.Friction = 0.3f;
 
 			body.SetMassFromShapes ();
-		}
+        }
+        Box box = new Box(body, hwidth, hheight);
+        boxList.Add(box);
+        return body;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		world.Step (1f / 60f, 6, 2);
+        testBody.SetPosition(new Vector2(testBody.GetPosition().x, 0));
+        Draw();
 	}
 
-	void OnDrawGizmos(){
-		foreach (Body body in bodyList) {
-			Fixture fixture = body.GetFixtureList();
-			if(fixture.ShapeType== ShapeType.CircleShape)
-			{
+    void Draw() {
+        foreach (var box in boxList)
+        {
+            box.Update();
+        }
+    }
+    int idx = 0;
+    void OnGUI() {
+        if (GUILayout.RepeatButton("xxxxxxxxxxxx"))
+        {
+            testBody.SetPosition(testBody.GetPosition() + Vector2.right*Time.deltaTime);  //.ApplyImpulse(Vector2.right * 10, testBody.GetPosition());
+        }
+    }
 
-			}
-			else if (fixture.ShapeType== ShapeType.PolygonShape){
-				PolygonShape ps = fixture.Shape as PolygonShape;
-				if(ps.Vertices.Length>=4)
-				for (int i = 0; i < 4; i++) {
-					int idx1 = i;
-					int idx2 = i+1;
-					if(idx2>=4)
-					{
-						idx2 = 0;
-					}
-					Vector2 pos = body.GetPosition();
-					Gizmos.DrawLine(pos+ ps.Vertices[idx1],pos+ps.Vertices[idx2]);
-				}
-			}
-		}
+	void OnDrawGizmos(){
+        //foreach (Body body in bodyList) {
+        //    Fixture fixture = body.GetFixtureList();
+        //    if(fixture.ShapeType== ShapeType.CircleShape)
+        //    {
+
+        //    }
+        //    else if (fixture.ShapeType== ShapeType.PolygonShape){
+        //        PolygonShape ps = fixture.Shape as PolygonShape;
+        //        if(ps.Vertices.Length>=4)
+        //        for (int i = 0; i < 4; i++) {
+        //            int idx1 = i;
+        //            int idx2 = i+1;
+        //            if(idx2>=4)
+        //            {
+        //                idx2 = 0;
+        //            }
+        //            Vector2 pos = body.GetPosition();
+        //            Gizmos.DrawLine(pos+ ps.Vertices[idx1],pos+ps.Vertices[idx2]);
+        //        }
+        //    }
+        //}
 	}
 }
 
---
+++ BoxPool.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class BoxPool  {
+
+    static BoxPool mPool = null;
+    public static BoxPool instance
+    {
+        get
+        {
+            if (mPool == null)
+            {
+                mPool = new BoxPool();
+            }
+            return mPool;
+        }
+    }
+
+    List<GameObject> objList = new List<GameObject>();
+    List<GameObject> useList = new List<GameObject>();
+    public GameObject Get() {
+        GameObject box;
+        if (objList.Count <= 0)
+        {
+            box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        }
+        else
+        {
+            box = objList[0];
+            objList.RemoveAt(0);
+            if (box == null)
+            {
+                return Get();
+            }
+        }
+        box.SetActive(true);
+        useList.Add(box);
+        return box;
+    }
+
+    public void Put(GameObject obj)
+    {
+        useList.Remove(obj);
+        objList.Add(obj);
+        obj.SetActive(false);
+    }
+}
